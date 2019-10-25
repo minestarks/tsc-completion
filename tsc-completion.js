145a2ab1b51f75f24e67b26b197c8e13e4b4f105
Mine Starks <mineyalc@microsoft.com>
2019-10-25 15:44:40 -0700

    some working version

--- tsc-completion.js
+++ tsc-completion.js
@@ -8,35 +8,42 @@ const ts = require('typescript');
 const compLine = process.env['COMP_LINE'] || "";
 const compPoint = parseInt(process.env['COMP_POINT']) || compLine.length;
 
-console.error(`invoked: compLine = ${compLine} compPoint = ${compPoint}`)
+// console.error(`invoked: compLine = ${compLine} compPoint = ${compPoint}`)
 
 if (compPoint == compLine.length) {
 
     const optionNameMap = ts.getOptionNameMap();
-    const names = Array.from(optionNameMap.optionNameMap.keys());
-    names.concat(Array.from(optionNameMap.shortOptionNames.keys()));
+    const optionMap = optionNameMap.optionNameMap;
+    const names = Array.from(optionMap.keys());
+    const shortOptions = optionNameMap.shortOptionNames;
+    names.concat(Array.from(shortOptions.keys()));
     const options = names.map(n => "--" + n);
 
     // need better parsing
-    const match = compLine.match(/tsc\s+([^\s]+\s+)*([^\s]+(\s?))$/);
-    const lastWord = match[2];
-    const spaceAfterLastWord = match[3];
+    const parts = compLine.split(/\s/).filter(s => s);
+    const lastWord = (parts.length > 1 && parts[parts.length - 1]) || "";
+    const lastWordComplete = compLine.endsWith(' ');
+    const currentOption = (lastWord.startsWith('-') && lastWordComplete)? lastWord : 
+        (!lastWordComplete && parts.length > 2 && parts[parts.length - 2].startsWith('-') ? parts[parts.length - 2] : "");
     
-    console.error(`lastWord: "${lastWord}" space: "${spaceAfterLastWord}"`);
+    // console.error(`lastWord: "${lastWord}" currentOption: "${currentOption}"`);
 
-    if (!spaceAfterLastWord) {
-        const compgenCmd = `bash -lic 'compgen -W "${options.join(' ')}" -- "${lastWord}"'`;
-        const results = child_process.execSync(compgenCmd).toString();
-    
-        process.stdout.write(results);
-    } else {
-        if (options.find(lastWord)) {
-            console.log('yeah!');
+    let completions = [];
+
+    if (currentOption) {
+        let option = currentOption.startsWith('--') ? currentOption.substr(2) : shortOptions.get(currentOption.substr(1));
+        const optionDesc = optionMap.get(option);
+        if (optionDesc && optionDesc.type instanceof Map) {
+            completions = Array.from(optionDesc.type.keys());
         }
-    }
-
+    } 
     
-
+    if(completions.length === 0) {
+        completions = options;
+    }
+    
+    completions.filter(o => lastWord && !lastWordComplete ? o.startsWith(lastWord) : true ).forEach(o => console.log(o));
+    
     //console.error(`results = ${results.split('\n').join(', ')}`);
 }
 // otherwise forget it too hard
